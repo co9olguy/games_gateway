bfb39c287b257c5ed44482fd3b1d603bf06743b4
co9olguy <co9olguy@users.noreply.github.com>
2016-01-27 21:11:20 +0100

    trying to change code in app.py again

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html','figure1.html','figure2.html')
+  
+@app.route('/figure1')
+def index():
+  return render_template('figure1.html')
+  
+@app.route('/figure2')
+def index():
+  return render_template('figure2.html')  
 
 if __name__ == '__main__':
   app.run(port=33507)
