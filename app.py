b3808dd44d5a217c9dbde3826aff813140908afc
co9olguy <co9olguy@users.noreply.github.com>
2016-04-02 11:42:52 -0400

    removed calls to psycopg2 and urllib libraries, added sqlalchemy dependency

--- app.py
+++ app.py
@@ -46,20 +46,20 @@ def get_filtered_games(filter_dict):
   # access postgres database
   DATABASE_URL = 'postgres://xsguljepueowms:IR7-TicHebWDkYr0WGZngcVsa5@ec2-23-21-157-223.compute-1.amazonaws.com:5432/d95o8es4f7241o'
 
-  import psycopg2
-  import urlparse
+  #import psycopg2
+  #import urlparse
 
-  urlparse.uses_netloc.append("postgres")
-  url = urlparse.urlparse(DATABASE_URL)
+  #urlparse.uses_netloc.append("postgres")
+  #url = urlparse.urlparse(DATABASE_URL)
 
-  # connect to database
-  ps_conn = psycopg2.connect(
-      database=url.path[1:],
-      user=url.username,
-      password=url.password,
-      host=url.hostname,
-      port=url.port
-  )
+  ## connect to database
+  #ps_conn = psycopg2.connect(
+  #    database=url.path[1:],
+  #    user=url.username,
+  #    password=url.password,
+  #    host=url.hostname,
+  #    port=url.port
+  #)
 
   test_query = "SELECT * FROM games WHERE minplayers <= {} AND maxplayers >= {} AND minplaytime >= {} AND maxplaytime <= {} AND minage >= {}".format(filter_dict['minplayers'], filter_dict['maxplayers'], filter_dict['minplaytime'],filter_dict['maxplaytime'], filter_dict['minage'])
 
